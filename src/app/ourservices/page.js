abe9494aa601857806cc50f23075a9ea881586e4
Ali Raza <137720587+malirazaansari@users.noreply.github.com>
2025-04-23 17:11:30 +0500

    footer and services page update

--- src/app/ourservices/page.js
+++ src/app/ourservices/page.js
@@ -7,8 +7,6 @@ import VehicleFleetSection from "@/components/VehicleFleetSection";
 import StatsHighlight from "@/components/StatsHighlight";
 import ClientLogosSection from "@/components/ClientLogosSection";
 import ServicesOfferSection from "@/components/ServicesOfferSection";
-import TripDetailsForm from "@/groups/TripDetailsForm";
-import VehicleSelection from "@/groups/VehicleSelection";
 
 export default function AboutPage() {
   return (
@@ -21,30 +19,6 @@ export default function AboutPage() {
         bgImage="/pagebanner.jpg"
       />
       <ServiceIntroSection  />
-
-{/* <TripDetailsForm
-            onPlaceSelected={handlePlaceSelected}
-            addViaPlace={addViaPlace}
-            isWaitAndReturn={isWaitAndReturn}
-            pickupPlace={pickupPlace}
-            dropoffPlace={dropoffPlace}
-            viaPlaces={viaPlaces}
-            onTripDetailsChange={setTripDetails}
-            onDateTimeChange={setSelectedDateTime}
-            isLoaded={isLoaded}
-            />
-            <VehicleSelection
-            onWaitAndReturnConfirmed={handleWaitAndReturnConfirmed}
-            isWaitAndReturnDisabled={isWaitAndReturnDisabled}
-            distance={distance}
-            onBookNow={handleBooking}
-            onVehicleSelect={setSelectedVehicle}
-            onExtrasChange={setExtras}
-            onPaymentMethodChange={setPaymentMethod}
-            selectedDateTime={selectedDateTime}
-            /> */}
-
-
       <VehicleFleetSection />
       <StatsHighlight />
       <ServicesOfferSection />
